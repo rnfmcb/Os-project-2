d4a195ff3bfdc811542fd43af3f24b62fbe164b5
rnfmcb <rachelfestervand@gmail.com>
2018-09-23 19:48:43 -0500

    Add files via upload

--- master.c
+++ master.c
@@ -5,20 +5,62 @@
 #include <stdlib.h> 
 #include <sys/wait.h> 
 #include <sys/shm.h> 
+#include <sys/stat.h> 
 
 int detachandremove(int shmid, void *shmaddr); 
 
 int main(int argc, char *argv[]){ 
 	pid_t childpid; 
 	int sleeptime = 2; 
+	int *sharedtotal; 
+	int c, p, n; 
 	
 	if (argc < 2) { //Check for valid amount of command line arguments 
 		fprintf (stderr, "Usage: %s command arg1 arg2...\n", argv[0]); 
 		return 1; 
 	} 
-	while (sleeptime != 0) { 
-	/*Allocate shared memory. If successful, shmat returns starting address of segment. Unsuccessful returns -1 and sets errno. */
-	void *shmat(int shmid, const void *shmaddr, int shmflg); 
+	//while (sleeptime != 0) { 
+	//Allocate shared memory.
+	if ((id =schmget(IPC_PRIVATE, sizeof(int), PERM))== 1) { 
+		perror("Failed to create shared memory segment"); 
+	return 1; 
+	}
+	if((sharedtotal = (int *) shmat(id, NULL)) == (void *)-1){ 
+		perror("Failed to attach shared memory segment"); 
+		if(shmct1(id, IPC_RMID, NULL) == -1){ 
+		perror("Failed to remove memory segmanet"); 
+	return 1; 
+	}
+	
+	
+	//Detach memory 
+	if (detachandremove(id, sharedtotal)== -1){ 
+		perror ("Failed to destroy shared memory segment"); 
+		return 1; 
+	} 
+	 
+	/* while ((c = getopt (argc, argv, "hpn:")) != -1)
+      switch (c) {
+        case 'h':
+			printf("Instructions on how program should be ran"); 
+	  break; 
+        case 's"; 
+         fprintf(stderr, "%s  error:", argv[0]);   
+          perror( "");
+          break; 
+        case 'n':
+            x = atoi(argv[2]); 
+	     for (i = 1; i < x; i++) 
+		if (childpid = fork()) 
+		break; 
+	  fprintf(stderr, "i:%d process ID:%ld parent ID:%ld child ID:%ld\n", 
+		i, (long)getpid(), (long)getppid(), (long)childpid); 
+         default: 
+		abort(); 
+      } */ 
+    
+return 0;  
+} 
 	
 	} 
 	if (sleeptime == 0){
@@ -38,6 +80,11 @@ int main(int argc, char *argv[]){
 		erro = error; 
 		return 0; 
 	} 
+	
+//Setup 2 second timer
+
+
+
 	
 //Signal handler to catch Ctrl C 
 void  INThandler(int sig){
